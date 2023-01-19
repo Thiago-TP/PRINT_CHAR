cae4bf90fc786817dce78972fb611ace9b5d4a83
Thiago-TP <thiagotomasdepaula@gmail.com>
2023-01-19 11:55:36 -0300

    bruhbruhbruhbruhbruh

--- Char.s
+++ Char.s
@@ -1,27 +1,29 @@
 .data
 
 .text
-li  s0, 300
-li  a0, -10
+MAIN:
+	li  s0, 255
+	li  a0, 0
 
-charLoop:
-    li  a7, 11
-    ecall
+	charLoop:
+    		li  a7, 11
+    		ecall           # imprime a0 como char
 
-    mv  t0, a0
-    li  a0, '\t'
-    ecall
+	    	mv  t0, a0
+	    	li  a0, '\t'
+	    	ecall           # imprime espaço
 
-    mv  a0, t0
-    li  a7, 1
-    ecall
+	    	mv  a0, t0
+	    	li  a7, 1
+	    	ecall           # imprime a0 como inteiro
 
-    li  a7, 11
-    li  a0, '\n'
-    ecall
+	    	li  a7, 11
+	    	li  a0, '\n'
+	    	ecall           # quebra de linha
 
-    mv  a0, t0
-    addi    a0, a0, 1
-    ble a0, s0, charLoop
+	    	mv  a0, t0
+	    	addi    a0, a0, 1
+	    	ble a0, s0, charLoop
 
-fpg:    j   fpg
+	li	a7, 10
+	ecall			# fim do programa
